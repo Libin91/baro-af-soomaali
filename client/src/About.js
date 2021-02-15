20240709d3a874c331acb97e5e3088390528e833
Libin91 <airani91@gmail.com>
2021-02-15 15:38:02 +0000

    improved responsiveness, and changed about page

--- client/src/About.js
+++ client/src/About.js
@@ -3,16 +3,9 @@ import React from 'react'
 function About() {
     return (
         <div>
-           <h1>Who are we?</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin vehicula tellus, eget facilisis lorem pulvinar ac. Praesent volutpat porttitor justo, eget rhoncus nisl. Quisque ut urna velit. Integer commodo magna eu consectetur eleifend.
-                 Quisque ultricies, ipsum quis facilisis porttitor, urna mi dictum lectus, eget tempor purus nunc et diam. Aenean congue massa ultricies rutrum fermentum. Nulla ut placerat augue. Fusce eu ultrices purus, at gravida augue. 
-                 Nam venenatis consectetur lectus, in finibus ipsum tempor at. Nam efficitur arcu sit amet tristique tempor. Pellentesque auctor leo sit amet diam tempor, eget ultrices ligula dictum. Nulla facilisi. Quisque suscipit nisl ut erat tincidunt molestie quis id leo.
-                Nullam sed mi sed nibh tempus malesuada sed ac nisl.</p>
-            <h1>What is this app?</h1>
-            <p>Quisque ornare tristique libero vel faucibus. In suscipit commodo libero in mollis. Curabitur mollis sit amet massa eget tempor. Proin et faucibus tortor.
-                Proin ac tortor et diam egestas convallis at eu orci. In mattis ante quis est sagittis, vitae dictum nisi maximus. Fusce sit amet scelerisque sapien, finibus pellentesque urna.
-                Pellentesque hendrerit feugiat lorem, ac mattis ipsum elementum ac.
-            </p> 
+           <h1>Who am I?</h1>
+            <p>My name is Asha, a student of the Black Codher bootcamp. Baro Af-Soomaali is my final project.</p>
+            
         </div>
     )
 }
